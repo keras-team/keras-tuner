6bd2cc56161fc0cb2c9ea572c218d246cc1b9abc
Elie <git@elie.net>
2018-05-10 12:42:35 -0700

    fixed demo to use the simpler distribution api

--- demo.py
+++ demo.py
@@ -18,29 +18,27 @@ def model_fn():
   IL_UNITS = Range(16, 32, 2)
   # Hidden layer
   L2_UNITS = Range(16, 32, 2)
-  L2_ACTIVATION = Choice(['relu', 'tanh'])
+  L2_ACTIVATION = Choice('relu', 'tanh')
   L2_OPTIONAL = Bool()
   # Last layer
   LL_UNITS = Fixed(1)
-  LL_ACTIVATION = Choice(['sigmoid', 'tanh'])
+  LL_ACTIVATION = Choice('sigmoid', 'tanh')
   # Compile options
-  OPTIMIZER = Choice([Adam(), SGD()])
-  LOSS = Choice(['binary_crossentropy', 'mse'])
-  METRICS = Fixed(['accuracy'])
+  OPTIMIZER = Choice(Adam(), SGD())
+  LOSS = Choice('binary_crossentropy', 'mse')
 
   model = Sequential()
   model.add(Dense(IL_UNITS, input_shape=(20,)))
   if L2_OPTIONAL:
     model.add(Dense(L2_UNITS, activation=L2_ACTIVATION))
   model.add(Dense(LL_UNITS, activation=LL_ACTIVATION))
-  model.compile(optimizer=OPTIMIZER, loss=LOSS, metrics=METRICS)
+  model.compile(optimizer=OPTIMIZER, loss=LOSS, metrics=['accuracy'])
   return model
 
 callbacks = [
   EarlyStopping(monitor='loss', min_delta=0.01, patience=1, verbose=0, mode='auto')
 ]
 METRIC_TO_REPORT = [('loss', 'min'), ('val_loss', 'min'), ('acc', 'max'), ('val_acc', 'max')] # which metrics to track accross the run and display
-
 mmodel = RandomSearch(model_fn, num_iterations=2, num_executions=2, metrics=METRIC_TO_REPORT)
 mmodel.search(x_train, y_train, epochs=10, validation_split=0.01, callbacks=callbacks)
 mmodel.statistics()
