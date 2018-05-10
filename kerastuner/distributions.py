91162b8a1068942748910c5f669dd35534aa31fe
Elie <git@elie.net>
2018-05-10 12:36:42 -0700

    distributions type fixed - Choice now don'

--- kerastuner/distributions.py
+++ kerastuner/distributions.py
@@ -9,12 +9,12 @@ def Bool():
     "Return a random bool"
     return random.choice([True, False])
 
-def Choice(selection):
+def Choice(*selection):
     """Return a random value from an explicit list of choice.
     Args:
-        selection (list): a list that contains explictly all the choices
+        *selection: a set of explicit choices
     Returns:
-        an element of the list
+        an element of the selecting
     """
     return random.choice(selection)
 
@@ -31,19 +31,27 @@ def Range(start, stop, increment=1):
       Don't generate the full range, do something more optimal
     """
     my_range = range(start, stop, increment)
-    return random.choice(my_range)
+    return int(random.choice(my_range))
 
-def Linear(start, stop, num_samples):
+def Linear(start, stop, num_buckets, precision=0):
     """Return a random value from a range which is linearly divided.
     Args:
         start (int/float): lower bound of the range
         stop (int/float): upper bound of the range
-        num_samples (int): incremental step
+        divider (int): into how many buckets should the range being divided in
+        precision (int): For float range. Round the result rounded to the nth decimal if needed. 0 means not rounded
     Returns:
         an element of the range
     
     Todo:
       Don't generate the full range, do something more optimal
     """
-    my_range = linspace(start, stop, num_samples)
-    return random.choice(my_range)
+    my_range = linspace(start, stop, num_buckets)
+    var = random.choice(my_range)
+    if isinstance(start, int):
+        return int(var)
+    else:
+        var = float(var)
+        if precision > 0:
+            var = round(var, precision + 1)
+        return var
--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     name="Kerastuner",
     version="0.1",
     description="Hypertuner for Keras",
+    author='Elie Bursztein',
+    author_email='elieb@google.com',
+    url='https://fixme',
+    license='Apache License 2.0',
     install_requires=open("requirements.txt").read().splitlines(),
     packages=find_packages(),
 )
