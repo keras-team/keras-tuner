ad5ed3edea069209fcfc5528fda1db066f866f2c
Elie <>
2018-06-15 09:33:38 -0700

    fixing binary upload

--- export_best_model.py
+++
@@ -1,8 +0,0 @@
-"""Export the best model utility
-
-Args:
-  idx: model idx for direct export
-  criteria: which criteria used to select model. e.g val_acc, loss ...
-  criteria_direction: min or max
-  criteria aggr: which statistical aggregation to use for the model:  mean, max, min, avg 
-"""
--- kerastuner/engine/keraslyzer.py
+++ kerastuner/engine/keraslyzer.py
@@ -7,11 +7,20 @@ def cloud_save(category, architecture, instance, local_path, execution=None, tra
   """Stores file remotely in a given GS bucket path."""
   if not gs_dir:
     return
-  ftype = '%s.json' % (execution or training_size or 'results')
+  if ".json" in local_path:
+    ftype = '%s.json' % (execution or training_size or 'results')
+    binary = ''
+  elif '.h5' in local_path:
+    ftype = '%s.h5' % (execution or training_size or 'results')
+    binary = 'b'
+  else:
+    Exception("unknown  file type for file:", local_path)
+
   remote_path = path.join(gs_dir, architecture, instance, category, ftype)
   if debug:
       cprint("[INFO] Uploading %s to %s" % (local_path, remote_path), 'cyan')
-  with file_io.FileIO(local_path, mode='r') as input_f:
-    with file_io.FileIO(remote_path, mode='w+') as output_f:
+
+  with file_io.FileIO(local_path, mode= 'r' + binary) as input_f:
+    with file_io.FileIO(remote_path, mode=  binary + 'w+') as output_f:
       output_f.write(input_f.read())
 
