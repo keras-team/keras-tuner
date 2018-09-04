e9d781d3776828aecbaebe134767fa017023d471
Elie <>
2018-09-04 10:58:23 -0700

    0.6.11 release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="Kerastuner",
-    version="0.6.9",
+    version="0.6.11",
     description="Hypertuner for Keras",
     author='Elie Bursztein',
     author_email='elieb@google.com',
