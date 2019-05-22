51f745e4b315779644dd061c731acdff4ae742ea
James Long <jamlong@google.com>
2019-05-22 10:39:46 -0700

    UltraBand updates.
    Change-Id: I6fd56a58210e304a0d91dd9f3b5c56cfc01e37f9

--- kerastuner/tuners/ultraband.py
+++ kerastuner/tuners/ultraband.py
@@ -16,11 +16,12 @@ from tqdm import tqdm
 
 from ..engine import Tuner
 
-class UltraBand(Tuner):
-  "UltraBand tuner"
 
-  def __init__(self, model_fn, **kwargs):
-    """ UltraBand hypertuner initialization
+class UltraBand(Tuner):
+    "UltraBand tuner"
+
+    def __init__(self, model_fn, **kwargs):
+        """ UltraBand hypertuner initialization
     Args:
       model_name (str): used to prefix results. Default: ts
 
@@ -45,134 +46,161 @@ class UltraBand(Tuner):
     FIXME:
      - Deal with early stop correctly
      - allows different halving ratio for epochs and models
-     - allows differnet type of distribution
-
+     - allows different type of distribution
     """
 
-    self.tuner_name = 'UltraBand'
-    self.halving_ratio = kwargs.get('halving_ratio', 3)
+        self.tuner_name = 'UltraBand'
+        self.halving_ratio = kwargs.get('halving_ratio', 3)
 
-    self.epoch_budget_expensed = 0
-    self.epoch_sequence = self.__geometric_seq(self.halving_ratio , self.min_epochs, self.max_epochs)
-    self.model_sequence = list(reversed(self.epoch_sequence)) # FIXME: allows to use another type of sequence
-    # clip epoch_sequence to ensure we don't train past max epochs
-    self.epoch_sequence[-1] = self.epoch_sequence[-1] - np.sum(self.epoch_sequence[:-1])
+        self.epoch_budget_expensed = 0
+        self.epoch_sequence = self.__geometric_seq(self.halving_ratio,
+                                                   self.min_epochs,
+                                                   self.max_epochs)
+        self.model_sequence = list(
+            reversed(self.epoch_sequence
+                     ))  # FIXME: allows to use another type of sequence
+        # clip epoch_sequence to ensure we don't train past max epochs
+        self.epoch_sequence[-1] = self.epoch_sequence[-1] - np.sum(
+            self.epoch_sequence[:-1])
 
-    self.band_costs = []
-    for i in range(1, len(self.epoch_sequence) + 1):
-      s1 = self.epoch_sequence[:i]
-      s2 = self.model_sequence[:i]
-      self.band_costs.append(np.dot(self.epoch_sequence[:i], self.model_sequence[:i])) #Note: General form, sepecialize sequences have faster way
-    self.loop_cost = np.sum(self.band_costs)
+        self.band_costs = []
+        for i in range(1, len(self.epoch_sequence) + 1):
+            s1 = self.epoch_sequence[:i]
+            s2 = self.model_sequence[:i]
+            self.band_costs.append(
+                np.dot(self.epoch_sequence[:i], self.model_sequence[:i])
+            )  # Note: General form, sepecialize sequences have faster way
+        self.loop_cost = np.sum(self.band_costs)
 
-    self.num_loops = self.epoch_budget / float(self.loop_cost)
-    self.num_bands = len(self.model_sequence)
-    self.loop_left = self.num_loops
+        self.num_loops = self.epoch_budget / float(self.loop_cost)
+        self.num_bands = len(self.model_sequence)
+        self.loop_left = self.num_loops
 
-    cprint('-=[UtraBand Tuning]=-', 'magenta')
-    cprint('|- Budget: %s' % self.epoch_budget, 'yellow')
-    cprint('|- Num models seq %s' % self.model_sequence, 'yellow' )
-    cprint('|- Num epoch seq: %s'%  self.epoch_sequence, 'yellow')
-    cprint('|- Bands', 'green' )
-    cprint('   |- numbers of band: %s' % len(self.model_sequence), 'green' )
-    cprint('   |- cost per band: %s'% self.band_costs, 'green')
-    cprint('|- Loops', 'blue')
-    cprint('   |- numbers of loop: %s' % self.num_loops, 'blue')
-    cprint('   |- cost per loop: %s'% self.loop_cost, 'blue')
+        cprint('-=[UtraBand Tuning]=-', 'magenta')
+        cprint('|- Budget: %s' % self.epoch_budget, 'yellow')
+        cprint('|- Num models seq %s' % self.model_sequence, 'yellow')
+        cprint('|- Num epoch seq: %s' % self.epoch_sequence, 'yellow')
+        cprint('|- Bands', 'green')
+        cprint('   |- numbers of band: %s' % len(self.model_sequence), 'green')
+        cprint('   |- cost per band: %s' % self.band_costs, 'green')
+        cprint('|- Loops', 'blue')
+        cprint('   |- numbers of loop: %s' % self.num_loops, 'blue')
+        cprint('   |- cost per loop: %s' % self.loop_cost, 'blue')
 
-    super(UltraBand, self).__init__(model_fn, **kwargs)
+        super(UltraBand, self).__init__(model_fn, **kwargs)
 
+    def search(self, x, y, **kwargs):
+        while self.loop_left > 0:
+            cprint(
+                'Budget:%s/%s - Loop %.2f/%.2f' %
+                (self.epoch_budget_expensed, self.epoch_budget, self.loop_left,
+                 self.num_loops), 'blue')
 
+            # Last (fractional) loop
+            if self.loop_left < 1:
+                # Reduce the number of models for the last fractional loop
+                self.model_sequence = self.__scale_loop(
+                    self.model_sequence, self.loop_left)
+                cprint('|- Scaled models seq %s' % self.model_sequence,
+                       'yellow')
+            for band_idx, num_models in enumerate(self.model_sequence):
+                band_total_cost = 0
+                cprint(
+                    'Budget:%s/%s - Loop %.2f/%.2f - Bands %s/%s' %
+                    (self.epoch_budget_expensed, self.epoch_budget,
+                     self.loop_left, self.num_loops, band_idx + 1,
+                     self.num_bands), 'green')
+                num_epochs = self.epoch_sequence[band_idx]
+                cost = num_models * num_epochs
+                self.epoch_budget_expensed += cost
+                band_total_cost += cost
+                # num_steps = len(self.model_sequence[band_idx + 1:])
 
-  def search(self,x, y, **kwargs):
-    while self.loop_left > 0:
-      cprint('Budget:%s/%s - Loop %.2f/%.2f' % (self.epoch_budget_expensed, self.epoch_budget, self.loop_left, self.num_loops), 'blue')
+                # Generate models
+                cprint('|- Generating %s models' % num_models, 'yellow')
+                model_instances = []
+                kwargs['epochs'] = num_epochs
+                if not self.dry_run:
+                    for _ in tqdm(range(num_models),
+                                  desc='Generating models',
+                                  unit='model'):
+                        model_instances.append(self.new_instance())
 
-      #Last (fractional) loop
-      if self.loop_left < 1:
-        #Reduce the number of models for the last fractional loop
-        self.model_sequence = self.__scale_loop(self.model_sequence, self.loop_left)
-        cprint('|- Scaled models seq %s' % self.model_sequence, 'yellow' )
-      for band_idx, num_models in enumerate(self.model_sequence):
-        band_total_cost = 0
-        cprint('Budget:%s/%s - Loop %.2f/%.2f - Bands %s/%s' % (self.epoch_budget_expensed, self.epoch_budget, self.loop_left, self.num_loops, band_idx + 1, self.num_bands), 'green' )
-        num_epochs = self.epoch_sequence[band_idx]
-        cost = num_models * num_epochs
-        self.epoch_budget_expensed += cost
-        band_total_cost += cost
-        num_steps = len(self.model_sequence[band_idx + 1:])
+                # Training here
+                cprint(
+                    '|- Training %s models for %s epochs' %
+                    (num_models, num_epochs), 'yellow')
+                kwargs['epochs'] = num_epochs
+                if self.dry_run:
+                    loss_values = model_instances = np.random.rand(
+                        num_models)  # real training instead
+                else:
+                    loss_values = []
+                    for instance in model_instances:
+                        results = instance.fit(x, y, **kwargs)
+                        loss_values.append(results.history['loss'][-1])
+                        self.record_results(idx=instance.idx)
 
-        ## Generate models
-        cprint('|- Generating %s models' % num_models, 'yellow')
-        model_instances = []
-        kwargs['epochs'] = num_epochs
-        if not self.dry_run:
-          for _ in tqdm(range(num_models), desc='Generating models', unit='model'):
-            model_instances.append(self.new_instance())
+                # climbing the band
+                for step, num_models in enumerate(
+                        self.model_sequence[band_idx + 1:]):
+                    num_epochs = self.epoch_sequence[step + band_idx + 1]
+                    cost = num_models * num_epochs
+                    self.epoch_budget_expensed += cost
+                    band_total_cost += cost
 
+                    # selecting best model
+                    band_models = self.__sort_models(
+                        model_instances, loss_values
+                    )  # Bogus replace 2nd term with the loss array
+                    band_models = band_models[:num_models]  # halve the m odels
 
+                    # train
+                    cprint(
+                        '|- Training %s models for an additional %s epochs' %
+                        (num_models, num_epochs), 'yellow')
+                    kwargs['epochs'] = num_epochs
+                    if self.dry_run:
+                        loss_values = model_instances = np.random.rand(
+                            num_models)  # real training instead
+                    else:
+                        loss_values = []
+                        for instance in model_instances:
+                            results = instance.fit(x, y, **kwargs)
+                            loss_values.append(results.history['loss'][-1])
+                            self.record_results(idx=instance.idx)
 
-        #Training here
-        cprint('|- Training %s models for %s epochs' % (num_models, num_epochs), 'yellow')
-        kwargs['epochs'] = num_epochs
-        if self.dry_run:
-          loss_values = model_instances = np.random.rand(num_models) #real training instead
-        else:
-          loss_values = []
-          for instance in model_instances:
-            results = instance.fit(x, y, **kwargs)
-            loss_values.append(results.history['loss'][-1])
-            self.record_results(idx=instance.idx)
+            self.loop_left -= 1
 
+    def __geometric_seq(self,
+                        ratio,
+                        min_num_epochs,
+                        max_num_epochs,
+                        scale_factor=1):
+        seq = []
+        for i in range(1, max_num_epochs + 1):  # will never go over
+            val = (scale_factor * ratio)**i
+            if val > max_num_epochs:
+                if seq[-1] != max_num_epochs:
+                    seq.append(max_num_epochs)
+                if seq[0] != min_num_epochs:
+                    seq = [min_num_epochs] + seq
+                return seq
+            if val > min_num_epochs:
+                seq.append(val)
 
-        #climbing the band
-        for step, num_models in enumerate(self.model_sequence[band_idx + 1:]):
-          num_epochs = self.epoch_sequence[step + band_idx + 1]
-          cost = num_models * num_epochs
-          self.epoch_budget_expensed += cost
-          band_total_cost += cost
+    def __scale_loop(self, seq, ratio):
+        "Scale the last loop to stay under budget"
+        scaled_seq = np.round(np.asarray(seq) * ratio).astype(
+            int)  # floor: don't want to go budget
+        return scaled_seq
 
-          # selecting best model
-          band_models = self.__sort_models(model_instances, loss_values) #Bogus replace 2nd term with the loss array
-          band_models = band_models[:num_models] # halve the m odels
-
-          #train
-          cprint('|- Training %s models for an additional %s epochs' % (num_models, num_epochs), 'yellow')
-          kwargs['epochs'] = num_epochs
-          if self.dry_run:
-            loss_values = model_instances = np.random.rand(num_models) #real training instead
-          else:
-            loss_values = []
-            for instance in model_instances:
-              results = instance.fit(x, y, **kwargs)
-              loss_values.append(results.history['loss'][-1])
-              self.record_results(idx=instance.idx)
-
-      self.loop_left -= 1
-
-  def __geometric_seq(self, ratio, min_num_epochs, max_num_epochs, scale_factor=1):
-    seq = []
-    for i in range(1, max_num_epochs + 1): #will never go over
-      val = (scale_factor * ratio)**i
-      if val > max_num_epochs:
-        if seq[-1] != max_num_epochs:
-          seq.append(max_num_epochs)
-        if seq[0] != min_num_epochs:
-          seq = [min_num_epochs] + seq
-        return seq
-      if val > min_num_epochs:
-        seq.append(val)
-
-  def __scale_loop(self, seq, ratio):
-    "Scale the last loop to stay under budget"
-    scaled_seq = np.round(np.asarray(seq) * ratio).astype(int) #floor: don't want to go budget
-    return scaled_seq
-
-  def __sort_models(self, models, loss_values):
-    "Return a sorted list of model by loss rate"
-    #FIXME remove early stops
-    indices = np.argsort( loss_values ) # recall loss is decreasing so use asc
-    sorted_models = []
-    for idx in indices:
-      sorted_models.append(models[idx])
-    return sorted_models
+    def __sort_models(self, models, loss_values):
+        "Return a sorted list of model by loss rate"
+        # FIXME remove early stops
+        indices = np.argsort(
+            loss_values)  # recall loss is decreasing so use asc
+        sorted_models = []
+        for idx in indices:
+            sorted_models.append(models[idx])
+        return sorted_models
