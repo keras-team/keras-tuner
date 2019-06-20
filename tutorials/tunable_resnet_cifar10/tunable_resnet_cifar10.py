385f6824a66b6b38dfd96d999acc6564f34a87e7
François Chollet <francois.chollet@gmail.com>
2019-06-20 16:56:38 -0700

    Remaster tutorials

--- tutorials/tunable_resnet_cifar10/tunable_resnet_cifar10.py
+++ tutorials/tunable_resnet_cifar10/tunable_resnet_cifar10.py
@@ -1,11 +1,11 @@
 # Copyright 2019 The Keras Tuner Authors
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     https://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,9 +16,8 @@
 
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.datasets import cifar10
-from kerastuner.applications.tunable_resnet import TunableResNet
-from kerastuner.tuners.randomsearch import RandomSearch
-
+from kerastuner.applications import HyperResNet
+from kerastuner import RandomSearch
 
 # Import the Cifar10 dataset.
 NUM_CLASSES = 10
@@ -27,31 +26,32 @@ y_train = to_categorical(y_train, NUM_CLASSES)
 y_test = to_categorical(y_test, NUM_CLASSES)
 
 # Import an hypertunable version of Resnet.
-model_fn = TunableResNet(
+hypermodel = HyperResNet(
     input_shape=x_train.shape[1:],
     num_classes=NUM_CLASSES)
 
 # Initialize the hypertuner: we should find the model that maximixes the
-# validation accuracy, training each model for ten epochs for a max of
-# 40 epochs of total training time.
+# validation accuracy, using 40 trials in total.
 tuner = RandomSearch(
-    model_fn,
-    objective='val_acc',
-    epoch_budget=40,
-    max_epochs=10,
-    project='Cifar10',
-    architecture='Resnet',
-    validation_data=(x_test, y_test),
-    max_params=50000000)
+    hypermodel,
+    objective='val_accuracy',
+    max_trials=40,
+    project_name='cifar10_resnet',
+    directory='test_directory')
 
 # Display search overview.
-tuner.summary()
+tuner.search_space_summary()
 
 # Performs the hypertuning.
-tuner.search(x_train, y_train, validation_split=0.01)
+tuner.search(x_train, y_train, epochs=10, validation_split=0.1)
 
 # Show the best models, their hyperparameters, and the resulting metrics.
 tuner.results_summary()
 
-# Export the top 2 models, in keras format format.
-tuner.save_best_models(num_models=1)
+# Retrieve the best model.
+best_model = tuner.get_best_models(num_models=1)[0]
+
+# Evaluate the best model.
+loss, accuracy = best_model.evaluate(x_test, y_test)
+print('loss:', loss)
+print('accuracy:', accuracy)
