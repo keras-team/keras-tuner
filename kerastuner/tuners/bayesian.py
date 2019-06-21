ad9eca2f0784622770fca2de839a2c8203ad21a2
Haifeng Jin <jhfjhfj1@gmail.com>
2019-06-20 19:27:29 -0700

    temp commit

--- kerastuner/tuners/bayesian.py
+++ kerastuner/tuners/bayesian.py
@@ -1,102 +1,50 @@
 import numpy as np
+import random
 from scipy import optimize as scipy_optimize
 from scipy import stats as scipy_stats
+from sklearn import gaussian_process
 
 from ..engine import tuner as tuner_module
 from ..engine import oracle as oracle_module
-
-
-def expected_improvement(X, X_sample, Y_sample, gpr, xi=0.01):
-    """ Computes the EI at points X based on existing samples X_sample and
-    Y_sample using a Gaussian process surrogate model.
-    Args:
-        X: Points at which EI shall be computed (m x d).
-        X_sample: Sample locations (n x d).
-        Y_sample: Sample values (n x 1).
-        gpr: A GaussianProcessRegressor fitted to samples.
-        xi: Exploitation-exploration trade-off parameter.
-    Returns:
-        Expected improvements at points X.
-    """
-    mu, sigma = gpr.predict(X, return_std=True)
-    mu_sample = gpr.predict(X_sample)
-
-    sigma = sigma.reshape(-1, X_sample.shape[1])
-
-    # Needed for noise-based model,
-    # otherwise use np.max(Y_sample).
-    # See also section 2.4 in [...]
-    mu_sample_opt = np.max(mu_sample)
-
-    with np.errstate(divide='warn'):
-        imp = mu - mu_sample_opt - xi
-        Z = imp / sigma
-        ei = imp * scipy_stats.norm.cdf(Z) + sigma * scipy_stats.norm.pdf(Z)
-        ei[sigma == 0.0] = 0.0
-
-    return ei
-
-
-def propose_location(acquisition, X_sample, Y_sample, gpr, bounds, n_restarts=25):
-    """ Proposes the next sampling point by optimizing the acquisition function.
-
-    Args:
-        acquisition: Acquisition function. X_sample: Sample locations (n x d).
-        Y_sample: Sample values (n x 1). gpr: A GaussianProcessRegressor fitted to
-            samples.
-    Returns:
-        Location of the acquisition function maximum.
-    """
-    dim = X_sample.shape[1]
-    min_val = 1
-    min_x = None
-
-    def min_obj(X):
-        # Minimization objective is the negative acquisition function
-        return -acquisition(X.reshape(-1, dim), X_sample, Y_sample, gpr)
-
-    # Find the best optimum by starting from n_restart different random points.
-    for x0 in np.random.uniform(bounds[:, 0], bounds[:, 1], size=(n_restarts, dim)):
-        res = scipy_optimize.minimize(min_obj, x0=x0, bounds=bounds, method='L-BFGS-B')
-        if res.fun < min_val:
-            min_val = res.fun[0]
-            min_x = res.x
-
-    return min_x.reshape(-1, 1)
-
-
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import ConstantKernel, Matern
-
-# Gaussian process with Mat??rn kernel as surrogate model
-m52 = ConstantKernel(1.0) * Matern(length_scale=1.0, nu=2.5)
-gpr = GaussianProcessRegressor(kernel=m52, alpha=noise ** 2)
-
-# Initialize samples
-X_sample = X_init
-Y_sample = Y_init
-
-# Number of iterations
-n_iter = 10
-
-for i in range(n_iter):
-    # Update Gaussian process with existing samples
-    gpr.fit(X_sample, Y_sample)
-
-    # Obtain next sampling point from the acquisition function (expected_improvement)
-    X_next = propose_location(expected_improvement, X_sample, Y_sample, gpr, bounds)
-
-    # Obtain next noisy sample from the objective function
-    Y_next = f(X_next, noise)
-
-    # Add sample to previous samples
-    X_sample = np.vstack((X_sample, X_next))
-    Y_sample = np.vstack((Y_sample, Y_next))
+from ..engine import hyperparameters as hp_module
 
 
 class BayesianOptimizationOracle(oracle_module.Oracle):
+    def __init__(self,
+                 init_samples=2,
+                 alpha=0.04,
+                 beta=0.01,
+                 seed=None):
+        super(BayesianOptimizationOracle, self).__init__()
+        self.init_samples = init_samples
+        self.alpha = alpha
+        self.beta = beta
+        self.seed = seed or random.randint(1, 1e4)
+        self._seed_state = self.seed
+        self._tried_so_far = set()
+        self._max_collisions = 20
+        self._num_trials = 0
+        self._score = {}
+        self._values = {}
+        self.gpr = gaussian_process.GaussianProcessRegressor(
+            kernel=gaussian_process.kernels.ConstantKernel(1.0),
+            alpha=self.alpha)
+
     def populate_space(self, trial_id, space):
-        pass
+        self.update_space(space)
+        # Generate enough samples before training Gaussian process.
+        if self._num_trials < self.init_samples or len(self._score) < 2:
+            self._num_trials += 1
+            values = self._new_trial()
+            self._values[trial_id] = values
+            return {'status': 'RUN', 'values': values}
+
+        x, y = self._training_data()
+        # Update Gaussian process with existing samples
+        self.gpr.fit(x, y)
+
+        values = self.propose_location(x.shape[1])
+        return self._convert(values)
 
     def report_status(self, trial_id, status):
         # TODO
@@ -111,6 +59,154 @@ class BayesianOptimizationOracle(oracle_module.Oracle):
         # TODO
         raise NotImplementedError
 
+    def result(self, trial_id, score):
+        self._score[trial_id] = score
+
+    def _new_trial(self):
+        """Fill a given hyperparameter space with values.
+
+        Returns:
+            A dictionary mapping parameter names to suggested values.
+            Note that if the Oracle is keeping tracking of a large
+            space, it may return values for more parameters
+            than what was listed in `space`.
+        """
+        collisions = 0
+        while 1:
+            # Generate a set of random values.
+            values = {}
+            for p in self.space:
+                values[p.name] = p.random_sample(self._seed_state)
+                self._seed_state += 1
+            # Keep trying until the set of values is unique,
+            # or until we exit due to too many collisions.
+            values_hash = self._compute_values_hash(values)
+            if values_hash in self._tried_so_far:
+                collisions += 1
+                if collisions > self._max_collisions:
+                    return None
+                continue
+            self._tried_so_far.add(values_hash)
+            break
+        return values
+
+    def _training_data(self):
+        x = []
+        y = []
+        for trial_id in self._values:
+            values = self._values[trial_id]
+            score = self._score[trial_id]
+
+            vector = [None] * len(values)
+            for name, value in values.items():
+                index = self._get_hp_index(name)
+                hp = self.space[index]
+                if isinstance(hp, hp_module.Choice):
+                    value = hp.values.index(value)
+                elif isinstance(hp, hp_module.Fixed):
+                    value = 0
+                vector[index] = value
+
+            x.append(vector)
+            y.append(score)
+
+        return np.array(x), np.array(y)
+
+    def _convert(self, vector):
+        values = {}
+        for index, value in enumerate(vector):
+            hp = self.space[index]
+            if isinstance(hp, hp_module.Choice):
+                value = hp.values[value]
+            elif isinstance(hp, hp_module.Fixed):
+                value = hp.value
+            values[hp.name] = value
+        return values
+
+    def _get_hp_index(self, name):
+        for index, hp in enumerate(self.space):
+            if hp.name == name:
+                return index
+        return None
+
+    def expected_improvement(self, X, X_sample):
+        """Computes the EI at points X based on existing samples X_sample and
+        Y_sample using a Gaussian process surrogate model.
+
+        Args:
+            X: Points at which EI shall be computed (m x d).
+            X_sample: Sample locations (n x d).
+
+        Returns:
+            Expected improvements at points X.
+        """
+        mu, sigma = self.gpr.predict(X, return_std=True)
+        mu_sample = self.gpr.predict(X_sample)
+
+        sigma = sigma.reshape(-1, X_sample.shape[1])
+
+        # Needed for noise-based model,
+        # otherwise use np.max(Y_sample).
+        # See also section 2.4 in [...]
+        mu_sample_opt = np.max(mu_sample)
+
+        with np.errstate(divide='warn'):
+            imp = mu - mu_sample_opt - self.beta
+            Z = imp / sigma
+            ei = imp * scipy_stats.norm.cdf(Z) + sigma * scipy_stats.norm.pdf(Z)
+            ei[sigma == 0.0] = 0.0
+
+        return ei
+
+    def propose_location(self, dim):
+        """ Proposes the next sampling point by optimizing the acquisition function.
+
+        Args:
+            dim: Int. x.shape[1]
+        Returns:
+            Location of the acquisition function maximum.
+        """
+        min_val = 1
+        min_x = None
+        bounds = []
+        for hp in self.space:
+            if isinstance(hp, hp_module.Choice):
+                bound = [0, len(hp.values)]
+            elif isinstance(hp, hp_module.Fixed):
+                bound = [0, 0]
+            else:
+                bound = [hp.min_value, hp.max_value]
+            bounds.append(bound)
+        bounds = np.array(bounds)
+
+        def min_obj(x):
+            # Minimization objective is the negative acquisition function
+            return -self.expected_improvement(x.reshape(-1, dim), x)
+
+        # Find the best optimum by starting from n_restart different random points.
+        n_restarts = 25
+        for x0 in np.random.uniform(bounds[:, 0], bounds[:, 1],
+                                    size=(n_restarts, dim)):
+            res = scipy_optimize.minimize(min_obj, x0=x0, bounds=bounds,
+                                          method='L-BFGS-B')
+            if res.fun < min_val:
+                min_val = res.fun[0]
+                min_x = res.x
+
+        return min_x.reshape(-1, 1)
+
 
 class BayesianOptimization(tuner_module.Tuner):
-    pass
+    def __init__(self,
+                 hypermodel,
+                 objective,
+                 max_trials,
+                 init_samples=2,
+                 seed=None,
+                 **kwargs):
+        oracle = BayesianOptimizationOracle(init_samples, seed)
+        super(BayesianOptimization, self, ).__init__(oracle=oracle,
+                                                     hypermodel=hypermodel,
+                                                     objective=objective,
+                                                     max_trials=max_trials,
+                                                     **kwargs)
