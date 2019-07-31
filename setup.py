4bfb6c0602c99bbb6d5124114ffb3bebe502298e
Haifeng Jin <jhfjhfj1@gmail.com>
2019-07-30 18:31:51 -0700

    specify numpy version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     license='Apache License 2.0',
     install_requires=[
         'tensorflow>=2.0.0-beta1',
-        'numpy',
+        'numpy==1.16.1',
         'tabulate',
         'terminaltables',
         'colorama',
