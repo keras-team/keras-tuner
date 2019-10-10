85cb7044c18cb20c915fe955cb76dbf52d1ee41e
Tom O'Malley <omalleyt@google.com>
2019-10-10 12:13:54 -0700

    Fix updating space in on_trial_end for MultiExecutionTuner

--- tests/kerastuner/tuners/randomsearch_test.py
+++ tests/kerastuner/tuners/randomsearch_test.py
@@ -11,3 +11,52 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import numpy as np
+import pytest
+import tensorflow as tf
+from kerastuner.engine import multi_execution_tuner
+from kerastuner.tuners import randomsearch
+
+
+@pytest.fixture(scope='function')
+def tmp_dir(tmpdir_factory):
+    return tmpdir_factory.mktemp('randomsearch_test', numbered=True)
+
+
+def test_update_space(tmp_dir):
+    def build_model(hp):
+        model = tf.keras.Sequential()
+        for i in range(hp.Int('layers', 0, 2)):
+            model.add(tf.keras.layers.Dense(
+                units=hp.Int('units_' + str(i), 2, 4, 2),
+                activation='relu'))
+        model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+        model.compile(
+            'adam',
+            loss='binary_crossentropy',
+            metrics=['accuracy'])
+        return model
+
+    class MyRandomSearch(randomsearch.RandomSearchOracle):
+
+        def _populate_space(self, trial_id):
+            result = super(MyRandomSearch, self)._populate_space(trial_id)
+            if 'values' in result:
+                result['values']['layers'] = 2
+            return result
+
+    tuner = multi_execution_tuner.MultiExecutionTuner(
+        oracle=MyRandomSearch(
+            objective='accuracy',
+            max_trials=1),
+        hypermodel=build_model,
+        directory=tmp_dir)
+
+    assert {hp.name for hp in tuner.oracle.get_space().space} == {'layers'}
+
+    x, y = np.ones((10, 10)), np.ones((10, 1))
+    tuner.search(x, y, epochs=1)
+
+    assert {hp.name for hp in tuner.oracle.get_space().space} == {
+        'layers', 'units_0', 'units_1'}
