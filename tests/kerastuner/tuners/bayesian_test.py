49f1fafaec39316b45f2bf1074b15c71b9d3a7aa
Tom O'Malley <omalleyt@google.com>
2019-12-09 23:15:26 -0800

    Reduce test flakiness

--- tests/kerastuner/tuners/bayesian_test.py
+++ tests/kerastuner/tuners/bayesian_test.py
@@ -282,7 +282,7 @@ def test_distributed_optimization(tmp_dir):
 
     def evaluate(hp):
         # Minimum at a=4, b=1, c=1e-3 with score=-1
-        return abs(hp['a'] - 4) - hp['b'] + abs(3 + math.log(hp['c'], 10))
+        return abs(hp['a'] - 4) - hp['b'] + 0.25*abs(3 + math.log(hp['c'], 10))
 
     oracle = bo_module.BayesianOptimizationOracle(
         objective=kt.Objective('score', 'min'),
@@ -312,4 +312,4 @@ def test_distributed_optimization(tmp_dir):
     assert best_trial.score < -0.8, best_hps.values
     assert np.isclose(best_hps['a'], 4, atol=atol, rtol=rtol)
     assert np.isclose(best_hps['b'], 1, atol=atol, rtol=rtol)
-    assert np.isclose(best_hps['c'], 1e-3, atol=atol, rtol=rtol)
+    assert np.isclose(best_hps['c'], 1e-3, atol=atol, rtol=1e-1)
