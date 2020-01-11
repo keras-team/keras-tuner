e5165935aae221320f033a84be956594983bf84f
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-01-11 10:40:02 +0000

    Results are different in python 2 and 3.

--- tests/kerastuner/randomness_test.py
+++ tests/kerastuner/randomness_test.py
@@ -1,11 +1,15 @@
 import random
 import numpy as np
 import tensorflow as tf
-
+import sys
 
 def test_seed_set_before_each_test():
     """Test that the tests are deterministic, locally and in the CI."""
     assert random.random() == 0.8444218515250481
     assert np.random.uniform() == 0.5488135039273248
-    random_tf_value = tf.random.uniform([1], dtype=tf.dtypes.float64)[0]
-    assert random_tf_value ==  0.3358018290373803
+
+    random_tf_value = tf.random.uniform([1], dtype=tf.dtypes.float64)[0].numpy()
+    if sys.version_info >= (3, 0):
+        assert random_tf_value == 0.3358018290373803
+    else:
+        assert random_tf_value == 0.3759982263246542
