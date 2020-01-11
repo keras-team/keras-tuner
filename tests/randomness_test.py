ff724fa11edf3f9e810c19c39869d34b18ebeff5
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-01-11 09:55:21 +0000

    Added a test to ensure determinist results.

---
+++ tests/randomness_test.py
@@ -0,0 +1,11 @@
+import random
+import numpy as np
+import tensorflow as tf
+
+
+def test_seed_set_before_each_test():
+    """Test that the tests are deterministic, locally and in the CI."""
+    assert random.random() == 0.8444218515250481
+    assert np.random.uniform() == 0.5488135039273248
+    random_tf_value = tf.random.uniform([1], dtype=tf.dtypes.float64)[0]
+    assert random_tf_value ==  0.3358018290373803
