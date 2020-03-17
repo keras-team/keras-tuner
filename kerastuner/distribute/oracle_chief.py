60b4ff184dc945d70d6d8fc1b47118340d68625a
Sergii Gladchuk <sergiigladchuk@gmail.com>
2020-03-17 11:54:26 +0100

    Following PyCodeStyle

--- kerastuner/distribute/oracle_chief.py
+++ kerastuner/distribute/oracle_chief.py
@@ -67,13 +67,15 @@ class OracleServicer(service_pb2_grpc.OracleServicer):
 
 
 class GracefulKiller:
-  kill_now = False
-  def __init__(self):
-    signal.signal(signal.SIGINT, self.exit_gracefully)
-    signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-  def exit_gracefully(self,signum, frame):
-    self.kill_now = True
+    kill_now = False
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self,signum, frame):
+        self.kill_now = True
 
 
 def start_server(oracle):
