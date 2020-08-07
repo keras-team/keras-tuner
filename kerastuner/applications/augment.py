a1ba70bd17776cb7d57f27e59811117d02b0293d
Yixing Fu <yxfu93@hotmail.com>
2020-08-06 20:38:01 -0400

    merge two kinds of augment into one

--- kerastuner/applications/augment.py
+++ kerastuner/applications/augment.py
@@ -38,41 +38,100 @@ TRANSFORMS = {
 
 class HyperImageAugment(hypermodel.HyperModel):
     """ Builds HyperModel for image augmentation.
+    Only supporting augmentations available in Keras preprocessing layers currently.
 
     # Arguments:
         input_shape: Optional shape tuple, e.g. `(256, 256, 3)`.
         input_tensor: Optional Keras tensor (i.e. output of
             `layers.Input()`) to use as image input for the model.
         rotate: A number between [0, 1], a list of two numbers between [0, 1]
-            or None. It configures the range of factor of rotation
-            transform in the augmentation. Default is 0.2.
+            or None. Configures the search space of the factor of random
+            rotation transform in the augmentation. A factor is chosen for each
+            trial. It sets maximum of clockwise and counterclockwise rotation
+            in terms of fraction of pi, among all samples in the trial.
+            Default is 0.5. When `rotate` is a single number, the search range is
+            [0, `rotate`].
+            The transform is off when set to None.
         translate_x: A number between [0, 1], a list of two numbers between [0, 1]
-            or None. It configures the range of factor of horizontal translation
-            transform in the augmentation. Default is 0.1.
+            or None. Configures the search space of the factor of random
+            horizontal translation transform in the augmentation. A factor is
+            chosen for each trial. It sets maximum of horizontal translation in
+            terms of ratio over the width among all samples in the trial.
+            Default is 0.3. When `translate_x` is a single number, the search range
+            is [0, `translate_x`].
+            The transform is off when set to None.
         translate_y: A number between [0, 1], a list of two numbers between [0, 1]
-            or None. It configures the range of factor of vertical translation
-            transform in the augmentation. Default is 0.1.
+            or None. Configures the search space of the factor of random vertical
+            translation transform in the augmentation. A factor is chosen for each
+            trial. It sets maximum of vertical translation in terms of ratio over
+            the height among all samples in the trial. Default is 0.3. When
+            `translate_y` is a single number ,the search range is [0, `translate_y`].
+            The transform is off when set to None.
         contrast: A number between [0, 1], a list of two numbers between [0, 1]
-            or None. It configures the range of factor of contrast
-            transform in the augmentation. Default is 0.1.
+            or None. Configures the search space of the factor of random contrast
+            transform in the augmentation. A factor is chosen for each trial. It
+            sets maximum ratio of contrast change among all samples in the trial.
+            Default is 0.3. When `contrast` is a single number, the search rnage is
+            [0, `contrast`].
+            The transform is off when set to None.
+        augment_layers: None, int or list of two ints, controlling the number
+            of augment applied. Default is 3.
+            When `augment_layers` is 0, all transform are applied sequentially.
+            When `augment_layers` is nonzero, or a list of two ints, a simple
+            version of RandAugment(https://arxiv.org/abs/1909.13719) is used.
+            A search space for 'augment_layers' is created to search [0,
+            `augment_layers`], or between the two ints if a `augment_layers` is
+            a list. For each trial, the hyperparameter 'augment_layers'
+            determines number of layers of augment transforms are applied,
+            each randomly picked from all available transform types with equal
+            probability on each sample.
         **kwargs: Additional keyword arguments that apply to all
             HyperModels. See `kerastuner.HyperModel`.
 
-    # Notes for keyword args that configures range of factor of transform:
-        rotate, translate_x, translate_y and contrast:
-            If two numbers are given, they represent minimum and maximum
-            factors; if one number is given, it represents maximum factor, and
-            minimum factor is 0; if None, the transform is disabled. All factors
-            are within [0, 1].
+    # Search space:
+        'factor_{transform}' for each of the transforms chosen:
+            Float, range is set through corresponding keyword argument.
+        'augment_layers' for number of transform applied to each sample:
+            Int, range is set through keyword argument `augment_layers`.
+            This is only active when `augment_layers` is non zero.
+
+    # Usage example:
+        ```
+        hm_aug = HyperImageAugment(input_shape=(32, 32, 3),
+                                   augment_layers=0,
+                                   rotate=[0.2, 0.3],
+                                   translate_x=0.1,
+                                   translate_y=None,
+                                   contrast=None)
+        ```
+        Then the hypermodel `hm_aug` will search 'factor_rotate' between [0.2, 0.3]
+        and 'factor_translate_x' between [0, 0.1]. These two augments are applied
+        on all samples with factor picked per each trial.
+        ```
+        hm_aug = HyperImageAugment(input_shape=(32, 32, 3),
+                                   translate_x=0.5,
+                                   translate_y=[0.2, 0.4]
+                                   contrast=None)
+        ```
+        Then the hypermodel `hm_aug` will search 'factor_rotate' between [0, 0.2],
+        'factor_translate_x' between [0, 0.5], 'factor_translate_y' between
+        [0.2, 0.4]. It will use RandAugment, searching 'augment_layers'
+        between [0, 3]. Each layer on each sample will be chosen from rotate,
+        translate_x and translate_y.
     """
     def __init__(self,
                  input_shape=None,
                  input_tensor=None,
-                 rotate=0.2,
-                 translate_x=0.1,
-                 translate_y=0.1,
-                 contrast=0.1,
+                 rotate=0.5,
+                 translate_x=0.3,
+                 translate_y=0.3,
+                 contrast=0.3,
+                 augment_layers=3,
                  **kwargs):
+
+        if input_shape is None and input_tensor is None:
+            raise ValueError('You must specify either `input_shape` or '
+                             '`intput_tensor`.')
 
         self.transforms = []
         self._register_transform('rotate', rotate)
@@ -82,10 +141,87 @@ class HyperImageAugment(hypermodel.HyperModel):
 
         self.input_shape = input_shape
         self.input_tensor = input_tensor
+
+        if augment_layers:
+            self.model_name = 'image_rand_augment'
+            try:
+                augment_layers_min = augment_layers[0]
+                augment_layers_max = augment_layers[1]
+            except TypeError:
+                augment_layers_min = 0
+                augment_layers_max = augment_layers
+            if not (isinstance(augment_layers_min, int) and
+                    isinstance(augment_layers_max, int)):
+                raise ValueError('Keyword argument `augment_layers` must be int,'
+                                 'but received {}. '.format(randaug_mag))
+
+            self.augment_layers_min = augment_layers_min
+            self.augment_layers_max = augment_layers_max
+        else:
+            # Separatedly tune and apply all augment transforms if
+            # `randaug_count` is set to 0.
+            self.model_name = 'image_augment'
+
         super(HyperImageAugment, self).__init__(**kwargs)
 
     def build(self, hp):
-        raise NotImplementedError
+        if self.input_tensor is not None:
+            inputs = keras.utils.get_source_inputs(self.input_tensor)
+            x = self.input_tensor
+        else:
+            inputs = layers.Input(shape=self.input_shape)
+            x = inputs
+
+        if self.model_name == 'image_rand_augment':
+            x = self._build_randaug_layers(x, hp)
+        else:
+            x = self._build_fixedaug_layers(x, hp)
+
+        model = keras.Model(inputs, x, name=self.model_name)
+        return model
+
+    def _build_randaug_layers(self, inputs, hp):
+        augment_layers = hp.Int('augment_layers',
+                                self.augment_layers_min,
+                                self.augment_layers_max,
+                                default=self.augment_layers_min)
+        x = inputs
+        for _ in range(augment_layers):
+            # selection tensor determines operation for each sample.
+            batch_size = tf.shape(x)[0]
+            selection = tf.random.uniform([batch_size, 1, 1, 1],
+                                          maxval=len(self.transforms),
+                                          dtype='int32')
+
+            for i, (transform, (f_min, f_max)) in enumerate(self.transforms):
+                # Factor for each transform is determined per each trial.
+                factor = hp.Float(f'factor_{transform}',
+                                  f_min,
+                                  f_max,
+                                  default=f_min)
+                if factor == 0:
+                    continue
+                transform_layer = TRANSFORMS[transform](factor)
+                x_trans = transform_layer(x)
+
+                # For each sample, apply the transform if and only if
+                # selection matches the transform index `i`
+                x = tf.where(tf.equal(i, selection), x_trans, x)
+        return x
+
+    def _build_fixedaug_layers(self, inputs, hp):
+        x = inputs
+        for transform, (factor_min, factor_max) in self.transforms:
+            transform_factor = hp.Float(f'factor_{transform}',
+                                        factor_min,
+                                        factor_max,
+                                        step=0.05,
+                                        default=factor_min)
+            if transform_factor == 0:
+                continue
+            transform_layer = TRANSFORMS[transform](transform_factor)
+            x = transform_layer(x)
+        return x
 
     def _register_transform(self, transform_name, transform_params):
         """Register a transform and format parameters for tuning the transform.
@@ -118,186 +254,3 @@ class HyperImageAugment(hypermodel.HyperModel):
 
         self.transforms.append((transform_name,
                                 (transform_factor_min, transform_factor_max)))
-
-
-class HyperImageFixedAugment(HyperImageAugment):
-    """An HyperModel for fixed policy augmentation.
-       A tunable hyperparameter is assigned to each of the
-       transforms chosen, and the transforms are applied
-       sequentially.
-       Only supporting augmentations in Keras preprocessing layers.
-
-    # Arguments:
-        input_shape: Optional shape tuple, e.g. `(256, 256, 3)`.
-        input_tensor: Optional Keras tensor (i.e. output of
-            `layers.Input()`) to use as image input for the model.
-        **kwargs: Additional keyword arguments that apply to all
-            HyperImageAugment. See `HyperImageAugment`.
-
-    # Search space:
-        'factor_{transform}' for each of the transforms chosen:
-            Float, range is set through corresponding keyword argument.
-
-    # Usage example:
-        ```
-        hm_aug = HyperImageFixedAugment(input_shape=(32, 32, 3),
-                                        translate_x=0.5,
-                                        translate_y=[0.2, 0.4]
-                                        contrast=0)
-        ```
-        Then the hypermodel `hm_aug` will search 'factor_rotate' in [0, 0.2],
-        'factor_translate_x' in [0, 0.5], 'factor_translate_y' in [0.2, 0.4].
-    """
-
-    def build(self, hp):
-        model = keras.Sequential(name='fixed_augment')
-
-        if self.input_tensor is not None:
-            model.add(self.input_tensor)
-        elif self.input_shape is not None:
-            model.add(keras.Input(shape=self.input_shape))
-
-        for transform, (factor_min, factor_max) in self.transforms:
-            transform_factor = hp.Float(f'factor_{transform}',
-                                        factor_min,
-                                        factor_max,
-                                        step=0.05,
-                                        default=factor_min)
-            if transform_factor == 0:
-                continue
-            transform_layer = TRANSFORMS[transform](transform_factor)
-            model.add(transform_layer)
-
-        return model
-
-
-class HyperImageRandAugment(HyperImageAugment):
-    """An HyperModel for RandAugment for image.
-       Based on https://arxiv.org/abs/1909.13719.
-       This augmentation randomly picks `randaug_count` augmentation transforms
-       from a pool of transforms for each sample, and control the augmentation
-       magnitude of all transforms using one parameter `randaug_mag`.
-       Only supporting augmentations in Keras preprocessing layers, which is a
-       subset of the larger pool of augmentation transforms in the original
-       paper.
-
-    # Arguments:
-        input_shape: Optional shape tuple, e.g. `(256, 256, 3)`.
-            One of `input_shape` or `input_tensor` must be
-            specified.
-        input_tensor: Optional Keras tensor (i.e. output of
-            `layers.Input()`) to use as image input for the model.
-            One of `input_shape` or `input_tensor` must be
-            specified.
-        randaug_mag: Number or list of 2 numbers. Default is 1. It
-            configures search space 'randaug_mag'. When there is one number x,
-            this hyperparameter is tuned betwee [0, x]; when there are two
-            numbers x and y, this hyperparameter is tuned between [x, y].
-        randaug_count: int or list of 2 int. Default is 4.  It
-            configures search space 'randaug_count'. When there is one number
-            x, this hyperparameter is tuned betwee [0, x]; when there are two
-            numbers x and y, this hyperparameter is tuned between [x, y].
-        **kwargs: Additional keyword arguments that apply to all
-            HyperImageAugment. See `HyperImageAugment`.
-
-    # Search space:
-        'randaug_mag': Float, set through `randaug_mag` keyword argument. Always
-            within [0, 1]. Controls the strength of each augmentation applied
-            to each image. For a transform with factor range set to [a, b],
-            factor in a trial will be a + (b - a) * randaug_mag.
-        'randaug_count': Int, set through `randaug_count` keyword argument. Always
-            nonnegative. Controls the number of augmentations layers applied to
-            each image.
-
-    # Usage example:
-        ```
-        hm_aug = HyperImageRandAugment(input_shape=(32, 32, 3),
-                                       translate_x=0.5,
-                                       translate_y=[0.2, 0.4]
-                                       contrast=0,
-                                       randaug_mag=[0.4, 0.6])
-        ```
-        Then the hypermodel `hm_aug` will search 'randaug_mag' in [0.2, 0.4],
-        'randaug_count' in [0, 5]. Contrast transform is turned off.
-        In a trial with randaug_mag = 0.5, we have factors for rotate,
-        translate_x, translate_y at 0.1, 0.25, 0.3 respectively.
-    """
-    def __init__(self,
-                 input_shape=None,
-                 input_tensor=None,
-                 randaug_mag=1,
-                 randaug_count=4,
-                 **kwargs):
-        if input_shape is None and input_tensor is None:
-            raise ValueError('You must specify either `input_shape` or '
-                             '`intput_tensor` when using HyperImageRandAugment.')
-
-        try:
-            randaug_mag_min = randaug_mag[0]
-            randaug_mag_max = randaug_mag[1]
-        except TypeError:
-            randaug_mag_min = 0
-            randaug_mag_max = randaug_mag
-        if not (isinstance(randaug_mag_min, (int, float)) and
-                isinstance(randaug_mag_max, (int, float))):
-            raise ValueError('Keyword argument `randaug_mag` must be float or int,'
-                             'but received {}. '.format(randaug_mag))
-
-        try:
-            randaug_count_min = randaug_count[0]
-            randaug_count_max = randaug_count[1]
-        except TypeError:
-            randaug_count_min = 0
-            randaug_count_max = randaug_count
-        if not (isinstance(randaug_count_min, int) and
-                isinstance(randaug_count_max, int)):
-            raise ValueError('Keyword argument `randaug_count` must be int,'
-                             'but received {}. '.format(randaug_mag))
-
-        self.randaug_mag_min = randaug_mag_min
-        self.randaug_mag_max = randaug_mag_max
-        self.randaug_count_min = randaug_count_min
-        self.randaug_count_max = randaug_count_max
-
-        super(HyperImageRandAugment, self).__init__(input_shape=input_shape,
-                                                    input_tensor=input_tensor,
-                                                    **kwargs)
-
-    def build(self, hp):
-        if self.input_tensor is not None:
-            inputs = keras.utils.get_source_inputs(self.input_tensor)
-            x = self.input_tensor
-        else:
-            inputs = layers.Input(shape=self.input_shape)
-            x = inputs
-
-        randaug_mag = hp.Float('randaug_mag',
-                               self.randaug_mag_min,
-                               self.randaug_mag_max,
-                               step=0.1,
-                               default=self.randaug_mag_min)
-        randaug_count = hp.Int('randaug_count',
-                               self.randaug_count_min,
-                               self.randaug_count_max,
-                               default=self.randaug_count_min)
-
-        for _ in range(randaug_count):
-            # selection tensor determines operation for each sample.
-            batch_size = tf.shape(x)[0]
-            selection = tf.random.uniform([batch_size, 1, 1, 1],
-                                          maxval=len(self.transforms),
-                                          dtype='int32')
-
-            for i, (transform, (f_min, f_max)) in enumerate(self.transforms):
-                factor = f_min + (f_max - f_min) * randaug_mag
-                if factor == 0:
-                    continue
-                transform_layer = TRANSFORMS[transform](factor)
-                x_trans = transform_layer(x)
-
-                # for each sample, apply the transform if and only if
-                # selection matches the transform index `i`
-                x = tf.where(tf.equal(i, selection), x_trans, x)
-
-        model = keras.Model(inputs, x, name='rand_augment')
-        return model
