8db00d66991e8380e7760e936f807a6974f26ec7
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2021-02-22 21:27:59 +0100

    Set seed before each test. (#210)
    * Set seed before each test.
    
    * Added a test to ensure determinist results.
    
    * Enabled all tests on travis.
    
    * Added keras autodoc in the CI
    
    * Revert changes.
    
    * Results are different in python 2 and 3.

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+import random
+import numpy as np
+import tensorflow as tf
+
+
+@pytest.fixture(autouse=True)
+def set_seeds_before_tests():
+    """Test wrapper to set the seed before each test.
+
+    This wrapper runs for all the tests in the test suite.
+    """
+    random.seed(0)
+    np.random.seed(0)
+    tf.random.set_seed(0)
+    yield
