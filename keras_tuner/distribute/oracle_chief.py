be682573c6f6be1e3f3e6dcac786a34ccac19d3b
mtian29 <mtian29@users.noreply.github.com>
2022-01-20 10:34:51 -0800

    Occasionally, when user runs tuning in distributed tuning mode, the worker never quits even after the chief already quits. This commit mitigates the issue by letting the chief to wait a longer time for all the subsequent grpc calls from the workers to be processed, so that the worker won't retry the grpc call forever. (#645)

--- keras_tuner/distribute/oracle_chief.py
+++ keras_tuner/distribute/oracle_chief.py
@@ -85,8 +85,10 @@ def start_server(oracle):
                 time.sleep(10)
 
             print(
-                "Oracle server on chief is exiting in 10s."
+                "Oracle server on chief is exiting in 40s."
                 "The chief will go on with post-search code."
             )
+            # Wait for another 30s for all the subsequent calls from workers to come
+            time.sleep(30)
             server.stop(10)
             break
