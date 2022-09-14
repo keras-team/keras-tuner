d0e3f40db992b3e6ee44824993f4150d570a5094
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2022-09-14 16:48:19 -0700

    Update hp_utils.py

--- keras_tuner/engine/hyperparameters/hp_utils.py
+++ keras_tuner/engine/hyperparameters/hp_utils.py
@@ -69,6 +69,7 @@ def check_sampling_arg(sampling, min_value, max_value):
     sampling_values = {"linear", "log", "reverse_log"}
     if sampling is None:
         sampling = "linear"
+    sampling = sampling.lower()
     if sampling not in sampling_values:
         raise ValueError(f"`sampling` must be one of {str(sampling_values)}")
     if sampling in {"log", "reverse_log"} and min_value <= 0:
