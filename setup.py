3196554c526407005ed34fdb113d267a544dfa59
Hanxiao Lyu <42114189+HanxiaoLyu@users.noreply.github.com>
2022-10-16 16:05:24 -0700

    Update the setup.py classifiers (#746)
    * Update the setup.py classifiers
    
    Added more supported Python versions in the `classifiers` of `setup.py` for better discoverability.
    
    * Also add python 3.11
    
    Co-authored-by: Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -52,6 +52,10 @@ setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: Unix",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: MacOS",
