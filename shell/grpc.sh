0df29143aad994b947af46b402af15d5ccf32f41
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2023-04-02 13:22:10 -0700

    upgrade codespaces to Python 3.10 (#888)

--- shell/grpc.sh
+++ shell/grpc.sh
@@ -1,3 +1,4 @@
 # Rebuilding rebuilding the gPRC protos
+# grpcio-tools==1.30.*
 python -m grpc_tools.protoc --python_out=. --grpc_python_out=. --proto_path=. keras_tuner/protos/keras_tuner.proto
 python -m grpc_tools.protoc --python_out=. --grpc_python_out=. --proto_path=. keras_tuner/protos/service.proto
