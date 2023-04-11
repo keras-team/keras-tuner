bf1921ef0ba60e685f3511176578ef066da09e1e
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2023-04-10 21:16:02 -0700

    fix container tf install (#895)

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 sudo pip install --upgrade pip
-sudo pip install -e ".[tests]"
+sudo pip install -e ".[tensorflow-cpu,tests]"
 echo "sh shell/lint.sh" > .git/hooks/pre-commit
 chmod a+x .git/hooks/pre-commit
