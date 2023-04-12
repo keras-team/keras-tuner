8dec93e4c1fd94dad5eda6f73d1aed5b129e995e
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2023-04-12 22:36:18 +0000

    support both protobuf v3 and v4

--- keras_tuner/protos/v4/__init__.py
+++ keras_tuner/protos/v4/__init__.py
@@ -11,26 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""KerasTuner protos."""
-
-from contextlib import contextmanager
-
-# v3 is generated with protobuf==3.20.3 and grpcio-tools==1.48.0
-# v4 is generated with protobuf==4.22.1 and grpcio-tools==1.53.0
-try:
-    from keras_tuner.protos import v4 as protos
-except ImportError:
-    from keras_tuner.protos import v3 as protos
-
-
-def get_proto():
-    return protos.keras_tuner_pb2
-
-
-def get_service():
-    return protos.service_pb2
-
-
-def get_service_grpc():
-    return protos.service_pb2_grpc
